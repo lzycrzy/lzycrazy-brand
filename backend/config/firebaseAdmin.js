5fd4704764a9055db3dcb217832e562422d08c92
BIKASH KUMAR SHARMA <bikashsharma5151@gmail.com>
2025-06-12 15:32:34 +0530

    updated

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,23 +1,54 @@
+// // backend/config/firebaseAdmin.js
+
+// import admin from 'firebase-admin';
+// import { readFileSync } from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+
+// // Get __dirname in ES module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Path to service.json
+// const serviceAccountPath = path.join(__dirname, '../dataBase/service.json');
+
+// // Read and parse JSON
+// const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+
+
+// // Initialize Firebase Admin SDK
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default admin;
+
+
+
 // backend/config/firebaseAdmin.js
 
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Get __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+if (!admin.apps.length) {
+  const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
 
-// Path to service.json
-const serviceAccountPath = path.join(__dirname, '../dataBase/service.json');
-
-// Read and parse JSON
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
