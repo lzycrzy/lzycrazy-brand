b9b841d5fbc37924f1bc343a2c0ab25e2d7d0b9e
israr8090 <mdistata772@gmail.com>
2025-06-25 08:06:05 +0530

    changes in app.js for port of forntend-5173 and admin-5174

--- backend/app.js
+++ backend/app.js
@@ -43,8 +43,8 @@ app.use(
 app.use(
   cors({
     origin: [
-              process.env.FRONTEND_URL || 'http://localhost:5174', 
-             process.env.DASHBOARD_URL || 'http://localhost:5173'
+              process.env.FRONTEND_URL || 'http://localhost:5173', 
+             process.env.DASHBOARD_URL || 'http://localhost:5174'
             ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
