9bf0747b6e64ad83e0d8e42629fdb91e884daf6d
israr8090 <mdistata772@gmail.com>
2025-07-01 11:40:50 +0530

    changes in .env

--- admin/src/lib/axios/axiosInstance.js
+++ admin/src/lib/axios/axiosInstance.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://lzycrazy-brand-backend.onrender.com/api/v1',
-  // baseURL: 'http://localhost:4000/api/v1',
-  baseURL: 'https://api.lzycrazy.com/api/v1',
+  baseURL: 'http://localhost:4000/api/v1',
+  // baseURL: 'https://api.lzycrazy.com/api/v1',
   withCredentials: true,
    headers: {
     'Content-Type': 'application/json',
