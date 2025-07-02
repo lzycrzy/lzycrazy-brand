60193de35373ca0a29a0f8907b34fd781e7393b0
israr8090 <mdistata772@gmail.com>
2025-07-02 19:18:00 +0530

    fixed

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -233,7 +233,7 @@ export function Sidebar() {
     {
       icon: FileBarChart,
       label: 'We are hiring',
-      path: '/applications'
+      path: '/Applications/Hiring'
     },
     { icon: FileBarChart, label: 'Client Enquiry', path: '/client-enquiry' },
     { icon: LogOut, label: 'Logout', path: '/logout' }
